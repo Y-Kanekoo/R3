5cde9b9013ff0ec94009808a5f7ab1d71a5fc17b
sho-1101 <sho1101kinako@gmail.com>
2024-10-15 13:47:00 +0900

    requirements.txt change

---
+++ myproject-master/myproject-master/myapp/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import DailyReportMorning
+
+class DailyReportMorningSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyReportMorning
+        fields = '__all__'  # すべてのフィールドをシリアライズ
--- myproject-master/myproject-master/myapp/urls.py
+++ myproject-master/myproject-master/myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import DailyReportMorningList
 
 urlpatterns = [
     path('reports/', views.show_daily_reports, name='show_daily_reports'),
     path('Daily_report_select/', views.Daily_report_select, name='Daily_report_select'),
+    path('api/reports/', DailyReportMorningList.as_view(), name='daily_report_list'),  # APIエンドポイントを追加
 ]
