adea722a82f2deec7d34acaaa1e22cefc17379c6
Y-Kanekoo <kaneko.yuki2001@gmail.com>
2024-10-21 12:14:45 +0900

    Remove .pyc file and update .gitignore

--- myproject-master/myproject-master/myapp/serializers.py
+++ myproject-master/myproject-master/myapp/serializers.py
@@ -1,23 +1,34 @@
 # myapp/serializers.py
 from rest_framework import serializers
-from .models import Employee, Questionnaire, DailyReport, DailyReportAnswer
+from .models import Employee, Questionnaire, DailyReport, DailyReportAnswer, Threshold
+
 
 class EmployeesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = '__all__'  # 全てのフィールドをシリアライズ   
+        fields = '__all__'  # 全てのフィールドをシリアライズ
+
 
 class QuestionnairesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Questionnaire
-        fields = '__all__'  # 全てのフィールドをシリアライズ   
+        fields = '__all__'  # 全てのフィールドをシリアライズ
+
 
 class DailyReportsSerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyReport
-        fields = '__all__'  # 全てのフィールドをシリアライズ   
+        fields = '__all__'  # 全てのフィールドをシリアライズ
+
 
 class DailyReportAnswersSerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyReportAnswer
-        fields = '__all__'  # 全てのフィールドをシリアライズ   
+        fields = '__all__'  # 全てのフィールドをシリアライズ
+
+
+class ThresholdsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Threshold
+        fields = '__all___'
--- myproject-master/myproject-master/myapp/views.py
+++ myproject-master/myproject-master/myapp/views.py
@@ -1,7 +1,7 @@
 # myapp/views.py
 from rest_framework import generics
-from .models import Employee, Questionnaire, DailyReport, DailyReportAnswer
-from .serializers import EmployeesSerializer,QuestionnairesSerializer,DailyReportsSerializer,DailyReportAnswersSerializer
+from .models import Employee, Questionnaire, DailyReport, DailyReportAnswer, Threshold
+from .serializers import EmployeesSerializer, QuestionnairesSerializer, DailyReportsSerializer, DailyReportAnswersSerializer, ThresholdsSerializer
 from django.shortcuts import render
 import requests
 
@@ -23,15 +23,19 @@ class EmployeesList(generics.ListAPIView):
     serializer_class = EmployeesSerializer
 
 # # APIからデータを取得し、テンプレートに表示するためのビュー
+
+
 def show_employees(request):
     # ローカルサーバー上のAPIエンドポイントからデータを取得
     response = requests.get('http://127.0.0.1:8000/api/EmployeesList/')
     data = response.json()  # APIから取得したデータをJSON形式で読み込む
     return render(request, 'myapp/show_employees.html', {'employees': data})
 
+
 class QuestionnairesList(generics.ListAPIView):
     queryset = Questionnaire.objects.all()
     serializer_class = QuestionnairesSerializer
+
 
 def show_questionnaires(request):
     response = requests.get('http://127.0.0.1:8000/api/QuestionnairesList/')
@@ -43,8 +47,10 @@ class Daily_report_answersList(generics.ListAPIView):
     queryset = DailyReportAnswer.objects.all()
     serializer_class = DailyReportAnswersSerializer
 
+
 def show_report_answers(request):
-    response = requests.get('http://127.0.0.1:8000/api/Daily_report_answersList/')
+    response = requests.get(
+        'http://127.0.0.1:8000/api/Daily_report_answersList/')
     data = response.json()  # APIから取得したデータをJSON形式で読み込む
     return render(request, 'myapp/show_report_answers.html', {'answers': data})
 
@@ -53,8 +59,21 @@ class Daily_reportsList(generics.ListAPIView):
     queryset = DailyReport.objects.all()
     serializer_class = DailyReportsSerializer
 
+
 def show_daily_reports(request):
     response = requests.get('http://127.0.0.1:8000/api/Daily_reportsList/')
-    question_response = requests.get('http://127.0.0.1:8000/api/QuestionnairesList/')
+    question_response = requests.get(
+        'http://127.0.0.1:8000/api/QuestionnairesList/')
     data = response.json()  # APIから取得したデータをJSON形式で読み込む
     return render(request, 'myapp/show_daily_reports.html', {'reports': data})
+
+
+class ThresholdsList(generics.ListAPIView):
+    queryset = Threshold.objects.all()
+    serializer_class = ThresholdsSerializer
+
+
+def show_Thresholds(request):
+    response = requests.get('http://127.0.0.1:8000/api/ThresholdsList/')
+    data = response.json()  # APIから取得したデータをJSON形式で読み込む
+    return render(request, 'myapp/show_thresholds.html', {'thresholds': data})
