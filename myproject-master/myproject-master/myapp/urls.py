fcced69186b3d3b8de4cf0ca0b76d077bb678058
sho-1101 <sho1101kinako@gmail.com>
2024-11-05 17:27:02 +0900

    最新版

--- myproject-master/myproject-master/myapp/urls.py
+++ myproject-master/myproject-master/myapp/urls.py
@@ -1,11 +1,13 @@
 # myapp/urls.py
 from django.urls import path
-from .views import EmployeesList, show_employees, QuestionnairesList, show_questionnaires, Daily_reportsList, show_daily_reports, Daily_report_answersList, show_report_answers,submit_answers
 from . import views
+from .views import EmployeesList, show_employees, employee_management, QuestionnairesList, show_questionnaires, Daily_reportsList, show_daily_reports, Daily_report_answersList, show_report_answers, submit_answers, submit_answers
 
 urlpatterns = [
     path('api/EmployeesList/', EmployeesList.as_view(), name='Employees_List'),
     path('show_employees/', show_employees, name='show_employees'),
+    path('employees/', employee_management, name='employee_management'),  # 一覧・追加・編集・削除を同じページで行う
+    path('employees/<int:employee_id>/', employee_management, name='employee_management_with_id'),
     path('api/QuestionnairesList/', QuestionnairesList.as_view(), name='Questionnaires_List'),
     path('show_questionnaires/', show_questionnaires, name='show_questionnaires'),
     path('api/Daily_reportsList/', Daily_reportsList.as_view(), name='Daily_reports_List'),
