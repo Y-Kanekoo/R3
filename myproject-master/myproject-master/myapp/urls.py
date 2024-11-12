60b6a7d155ef58106919063ca1fab33571aaa127
sho-1101 <sho1101kinako@gmail.com>
2024-11-12 14:59:11 +0900

    csv出力

--- myproject-master/myproject-master/myapp/urls.py
+++ myproject-master/myproject-master/myapp/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path('user-list/', user_list, name='user_list'),
     path('update-user/<int:user_id>/', update_user, name='update_user'),
+
+    path('export_reports_csv/', views.export_reports_csv, name='export_reports_csv'),
 ]
